97b347711d7a4b26a81905de64c64859ea72c366
ElenaLenivtseva <lenivtseva.lena@yandex.ru>
2024-02-21 22:52:47 +0700

    Добавлены css-переменные, заданы стили для базовой разметки

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h1 className='title'>Hello world</h1>
+      <div className="box">
+        <h2>This is a box</h2>
+      </div>
+    </div>
+  )
+}
+
+export default App
