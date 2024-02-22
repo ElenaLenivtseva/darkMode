5dcd92e8b36d61b291ce48082fad5e1416161a75
ElenaLenivtseva <lenivtseva.lena@yandex.ru>
2024-02-22 20:31:31 +0700

    реализован toogle

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React,{useState} from 'react'
+import useLocalStorage from 'use-local-storage'
 import './App.css'
+import Toggle from './components/Toggle';
 
 const App = () => {
+  const preference = window.matchMedia("(prefers-color-scheme: dark)").matches;
+cost [isDark, setIsDark] = useLocalStorage('isDark', preference);
+
   return (
-    <div className='App' data-theme='dark'>
+    <div className='App' data-theme={isDark?'dark':''}>
+      <Toggle isChecked={isDark} handleChange={()=> setIsDark(!isDark)}/>
       <h1 className='title'>Hello world</h1>
       <div className="box">
         <h2>This is a box</h2>
